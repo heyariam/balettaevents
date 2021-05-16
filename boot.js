4d2354994e97a89c4cd059d9b9edc3fbc38e4bb0
ariavalosm <arianna.avalos15@gmail.com>
2021-05-16 01:31:34 -0500

    aster

---
+++ boot.js
@@ -0,0 +1,36 @@
+var maxHeight = 400;
+
+$(function(){
+
+    $(".dropdown > li").hover(function() {
+    
+         var $container = $(this),
+             $list = $container.find("ul"),
+             $anchor = $container.find("a"),
+             height = $list.height() * 1.1,       
+             multiplier = height / maxHeight;     
+        
+                   
+        $container.data("origHeight", $container.height());
+        
+        $anchor.addClass("hover");
+         
+        $list
+            .show()
+            .css({
+                paddingTop: $container.data("origHeight")
+            });
+        
+           if (multiplier > 1) {
+            $container
+                .css({
+                    height: maxHeight,
+                    overflow: "hidden"
+                })
+                .mousemove(function(e) {
+                    var offset = $container.offset();
+                    var relativeY = ((e.pageY - offset.top) * multiplier) - ($container.data("origHeight") * multiplier);
+                    if (relativeY > $container.data("origHeight")) {
+                        $list.css("top", -relativeY + $container.data("origHeight"));
+                    };
+   
